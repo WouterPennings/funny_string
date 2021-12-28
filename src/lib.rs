d9440d65d100a1cc923e6fcbe45f5f634c5d3278
WouterPennings <wouterpennings@gmail.com>
2021-12-28 22:00:41 +0100

    feat: replace functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
-fn get_char_from_string(string: &str, index: i32) -> char {
+pub fn index_string(string: &str, index: i32) -> char {
     return string.chars().nth(index as usize).unwrap();
 }
 
-fn get_substring(mut text: &str, begin: i32, end: i32) -> String {
-    return text.chars().skip(begin as usize).take((end) as usize).collect();
+pub fn substring(string: &str, begin: i32, end: i32) -> String {
+    return string.chars().skip(begin as usize).take((end) as usize).collect();
+}
+
+pub fn replace_char(string: &str, index: i32, replacement: char) -> String {
+    let x: usize = index as usize;
+    let first = &string[..x];
+    let second = &string[x + 1..];
+    return format!("{}{}{}", first, replacement, second);
+}
+
+pub fn replace_substring(string: &str, begin: i32, end: i32, replacement: char) -> String {
+    let begin: usize = begin as usize;
+    let end: usize = end as usize;
+    let mut replacement_substring = "".to_string();
+    for _ in 0..end-begin {
+        replacement_substring.push(replacement);
+    }
+    let first = &string[..begin];
+    let second = &string[end..];
+    return format!("{}{}{}", first, replacement_substring, second);
 }
